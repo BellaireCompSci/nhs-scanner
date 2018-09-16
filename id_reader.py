b2ece352e302b16beba2c726a8b2d9afdaa557e7
Ryan Knightly <reknightly@gmail.com>
2018-09-16 00:43:46 -0500

    Change date format, handle no name found, change ids to ints

--- id_reader.py
+++ id_reader.py
@@ -9,53 +9,79 @@
 import csv
 import time
 
-STUDENT_LIST_FILE_NAME = 'student_list.csv'
-RECORD_FILE = 'attendance-{date}.csv'.format(date=time.strftime("%Y-%m-%d %H:%M"))
+
+STUDENT_LIST_FILE = 'student_list.csv'
+ATTENDANCE_FILE = f'Attendance_{time.strftime("%Y-%m-%d_%I-%M%p")}.csv'
+
 
 def main():
     student_data = read_students()
-    while True:
-        date, scanned_name, scanned_num = scan_student(student_data)
-        record_student(date, scanned_name, scanned_num)
+    print('\n READY TO SCAN. \n')
+    create_attendance_file()
 
+    while True:
+        result = scan_student(student_data)
+        if result is None:
+            continue
+        else:
+            date, scanned_name, scanned_num = result
+            record_student(date, scanned_name, scanned_num)
 
 def read_students():
     student_list = {}
 
-    with open(STUDENT_LIST_FILE_NAME, mode='r') as csv_file:
+    with open(STUDENT_LIST_FILE, mode='r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
             # First row is headers (column names)
             if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
+                print(f'Column names are {", ".join(row)}') # TODO: remove
                 line_count += 1
             # Other rows contain Name, ID of students
             else:
                 student_name = row[0]
-                student_id = row[1] # Keep as string to avoid conversion errors
+                student_id = int(row[1])
 
                 student_list[student_id] = student_name
 
-                print(f'Student: {student_name} ID: {student_id}.')
+                print(f'Student: {student_name} ID: {student_id}.') # TODO: remove
                 line_count += 1
 
-    print(f'Processed data for {line_count} students.')
+    print(f'Processed data for {line_count} students.') # TODO: remove
     return student_list
 
 
 def scan_student(student_list):
-    scanned_num = input('Scan student ID: ')
-    scanned_name = student_list[scanned_num]
-    date = time.strftime("%Y-%m-%d %H:%M")
+    scanned_num = input('[Scan/Enter] Student ID: ')
+
+    try:
+        scanned_num = int(scanned_num)
+    except ValueError:
+        print('[WARNING] Invalid ID number!')
+        return None
+
+    if scanned_num in student_list:
+        scanned_name = student_list[scanned_num]
+    else:
+        print('[WARNING] Student name not found.')
+        scanned_name = "UNKNOWN"
+
+    print(f'Welcome, {scanned_name}!')
+    date = time.strftime("%Y-%m-%d_%I-%M%p")
 
     return date, scanned_name, scanned_num
 
 
-def record_student(date, name, id_num):
-    with open(RECORD_FILE, mode='a') as employee_file:
-        student_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+def create_attendance_file():
+    with open(ATTENDANCE_FILE, mode='w') as file:
+        student_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        student_writer.writerow(["Date-Time", "Name", "ID Number"])
 
+
+def record_student(date, name, id_num):
+    with open(ATTENDANCE_FILE, mode='a') as file:
+        student_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         student_writer.writerow([date, name, id_num])
 
 
