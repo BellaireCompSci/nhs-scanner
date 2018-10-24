a10d14dd8159d424486b018481cb05dacdd2dd2f
Ryan Knightly <reknightly@gmail.com>
2018-10-24 14:47:06 -0500

    Update to new file format

--- src/id_reader.py
+++ src/id_reader.py
@@ -2,7 +2,7 @@ import csv
 import time
 
 
-STUDENT_LIST_FILE = 'data/student_list.csv'
+STUDENT_LIST_FILE = 'data/NHSMasterJrsSrs.csv'
 ATTENDANCE_FILE = f'Attendance_{time.strftime("%Y-%m-%d_%I-%M%p")}.csv'
 
 
@@ -21,8 +21,8 @@ def main():
         if result is None:
             continue
         else:
-            date, scanned_name, scanned_num = result
-            record_student(date, scanned_name, scanned_num)
+            date, last_name, first_name, grade, scanned_num = result
+            record_student(date, last_name, first_name, grade, scanned_num)
 
 
 def read_students():
@@ -30,7 +30,14 @@ def read_students():
     Reads the student data from the STUDENT_LIST_FILE
     Data should be in a csv file with the columns (name, id)
 
-    :returns: A dictionary of all the students in the form {name: id}
+    :returns: A dictionary of all the students in the form 
+    {
+    id: {
+        'last-name': '',
+        'first-name': '',
+        'grade': 0,
+        }
+    }
     '''
     all_students = {}
 
@@ -38,19 +45,28 @@ def read_students():
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
-            if line_count == 0: # First row is headers (column names)
-                print(f'Column names are {", ".join(row)}') # TODO: remove
-                line_count += 1
+            line_count += 1
+            if line_count == 1: # First row is headers (column names)
+                # print(f'Column names are {", ".join(row)}') # TODO: remove
+                continue
+            elif len(row[0]) == 0: # Blank rows (at end of file)
+                line_count -= 1
+                break
             else:   # Other rows contain Name, ID of students
-                student_name = row[0]
-                student_id = int(row[1])
+                student_lastname = row[0]
+                student_firstname = row[1]
+                student_grade = int(row[2])
+                student_id = int(row[3])
 
-                all_students[student_id] = student_name
+                student_data = {'last-name': student_lastname, 
+                                'first-name': student_firstname,
+                                'grade': student_grade}
 
-                print(f'Student: {student_name} ID: {student_id}.') # TODO: remove
-                line_count += 1
+                all_students[student_id] = student_data
 
-    print(f'Found data for {line_count} students.') # TODO: remove
+                # print(f'last-name: {student_lastname}, first-name: {student_firstname}, grade: {student_grade}, ID: {student_id}.') # TODO: remove
+
+    print(f'Using data for {line_count-1} students.') 
     return all_students
 
 
@@ -60,7 +76,7 @@ def scan_student(all_students):
     The input can be typed in manually or scanned with a USB scanner
 
     :param all_students: a dictionary of form {name: id} for all the students 
-    :returns: date-time of scan, name of student scanned, student ID number scanned
+    :returns: date-time of scan, last name, first name, grede, and ID number of student scanned
     '''
     scanned_num = input('[Scan/Enter] Student ID: ')
 
@@ -71,15 +87,21 @@ def scan_student(all_students):
         return None
 
     if scanned_num in all_students:
-        scanned_name = all_students[scanned_num]
+        student_data = all_students[scanned_num]
+
+        last_name = student_data['last-name']
+        first_name = student_data['first-name']
+        grade = student_data['grade']
     else:
         print('[WARNING] Student name not found.')
-        scanned_name = "UNKNOWN"
+        last_name = "UNKNOWN"
+        first_name = "UNKNOWN"
+        grade = -1
 
-    print(f'Welcome, {scanned_name}!')
+    print(f'Welcome, {first_name} {last_name}!\n')
     date = time.strftime("%Y-%m-%d_%I-%M%p")
 
-    return date, scanned_name, scanned_num
+    return date, last_name, first_name, grade, scanned_num
 
 
 def create_attendance_file():
@@ -90,18 +112,19 @@ def create_attendance_file():
     '''
     with open(ATTENDANCE_FILE, mode='w') as file:
         student_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        student_writer.writerow(["Date-Time", "Name", "ID Number"])
+        student_writer.writerow(["Date-Time", "Last Name", "First Name", "Grade", "ID Number"])
 
 
-def record_student(date, name, id_num):
+def record_student(date, last_name, first_name, grade, id_num):
     '''
     Records the attendance of one student in the attendance file
 
     :param date: a string of the date of the scan
-    :param name: a string of the name of the student
+    :param last_name: a string of the last name of the student
+    :param first_name: a string of the first name of the student
     :param id_num: an int of the scanned ID number
     :returns: None
     '''
     with open(ATTENDANCE_FILE, mode='a') as file:
         student_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        student_writer.writerow([date, name, id_num])
+        student_writer.writerow([date, last_name, first_name, grade, id_num])
